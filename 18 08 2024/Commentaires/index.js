1fd175c8fccf84153332742d92f8c16c10182da4
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-08-18 13:27:17 +0200

    test

--- 18 08 2024/Commentaires/index.js
+++ 18 08 2024/Commentaires/index.js
@@ -1,11 +1,30 @@
-let texte = document.getElementById("comment-input");
+const texte = document.getElementById("comment-input");
 
-let bouton = document.getElementById("add-comment-btn");
-
+const bouton = document.getElementById("add-comment-btn");
+//works with react
+//bouton.setAttribute("disabled", "true");
+//bouton.classList.add("disabled");
+//need always listener
+/*texte.addEventListener("keydown", () => {
+  console.log(texte.value.trim());
+  if (texte.value.trim()) {
+    bouton.setAttribute("disabled", "true");
+    bouton.classList.add("disabled");
+  } else {
+    bouton.setAttribute("disabled", "false");
+    bouton.classList.remove("disabled");
+  }
+});*/
 bouton.addEventListener("click", () => {
   let message = document.getElementById("comment-input");
-  if (message.ariaValueMax.trim()) {
+  if (message.value.trim()) {
     const parent = document.getElementById("comment-list");
+    const newElement = document.createElement("li");
+    newElement.textContent = message.value;
+    const commentList = document.getElementById("comment-list");
+    commentList.appendChild(newElement);
+    message.value = "";
   } else {
+    alert("Please type a message");
   }
 });
